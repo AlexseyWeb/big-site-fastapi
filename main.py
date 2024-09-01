153bb5c2f73cc29da94b84e95259d8009aa2d0c5
AlexseyWeb <alongenet32@gmail.com>
2024-09-02 00:01:24 +0300

    added list my_post, refactoring routes posts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -10,15 +11,19 @@ class Post(BaseModel):
     published: bool = True 
     rating: Optional[int] = None 
 
+my_posts = [{"title": "First post ", "content": "This is a first post on site", "id": 1}, {"title": "Second post", "content": "A second post on site", "id": 2}]
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my api!!"}
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This is your post!"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 async def create_posts(new_post:Post):
-    print(new_post)
+    post_dict = new_post.dict()
+    post_dict["id"] = randrange(0, 100000)
+    my_posts.append(post_dict)
     return {"data": new_post}
