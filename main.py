42903b034df3fef2775733dcd308162a5bf4bf49
AlexseyWeb <alongenet32@gmail.com>
2024-09-01 22:02:28 +0300

    added routes and pydantic model Post

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True 
+    rating: Optional[int] = None 
+
 @app.get("/")
 async def root():
-    return {"message": "Start Project!"}
+    return {"message": "Welcome to my api!!"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your post!"}
+
+@app.post("/createposts")
+async def create_posts(new_post:Post):
+    print(new_post)
+    return {"data": new_post}
