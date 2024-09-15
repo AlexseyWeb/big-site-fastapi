d4fc3cfff7720691aa0500747d5da51315a3f36c
AlexseyWeb <alongenet32@gmail.com>
2024-09-15 22:14:59 +0300

    added schemas for pydantic

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True 
+
+class PostCreate(PostBase):
+    pass 
+
+class Post(BaseModel):
+    id: int 
+    title: str 
+    content: str 
+    published: bool
+    created_at: datetime
+
+    class Config:
+        orm_mode = True 
+   
+
+class InfoAboutAPI(BaseModel):
+    version: float
+    description: str 
+    authors: str
