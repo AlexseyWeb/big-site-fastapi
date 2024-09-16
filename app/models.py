942152cf44bd678b0a7f7cc625443f1a75a1afa0
AlexseyWeb <alongenet32@gmail.com>
2024-09-16 22:13:58 +0300

    added Users model

--- app/models.py
+++ app/models.py
@@ -12,4 +12,8 @@ class Post(Base):
     published = Column(Boolean, server_default="TRUE", nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     
-
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
