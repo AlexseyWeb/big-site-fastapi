495ff02b84643c22e1b3653317e14ef4e03f89b1
AlexseyWeb <alongenet32@gmail.com>
2024-09-24 22:50:11 +0300

    update

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,9 @@
+"""
+- This is file working with path
+- Routers with /users/
+
+"""
+
 from .. import models, schemas, utils
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
