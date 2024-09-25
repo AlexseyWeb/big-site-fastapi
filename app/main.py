ca58ffd0d44a032374650965e3af95a24c074634
AlexseyWeb <alongenet32@gmail.com>
2024-09-25 21:01:18 +0300

    created auth for token

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from . import models, schemas
 from .database import engine
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -22,6 +22,7 @@ while True:
         print("Database connection is wrong! ", err)
         time.sleep(2)
 
+app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(user.router)
 
