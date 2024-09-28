041f2e26298bddf1853ec39c9247b392015679c8
AlexseyWeb <alongenet32@gmail.com>
2024-09-28 20:40:24 +0300

    added token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
@@ -12,4 +12,5 @@ async def login(user_credentials:schemas.UserLogin, db: Session = Depends(databa
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid password")
     
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"token": access_token}
